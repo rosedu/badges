4f4030f37791010495e8f46daebf4ba5ee4a59f7
Alex Morega <alex@grep.ro>
2013-03-06 11:39:36 +0200

    show badges for a person

--- viewer/__init__.py
+++ viewer/__init__.py
@@ -8,10 +8,26 @@ grants = dump.read_grants(path(__name__).parent / 'grant')
 viewer = flask.Blueprint('viewer', __name__, template_folder='templates')
 
 
+def all_badges():
+    for badge_id, item_list in grants.iteritems():
+        for item in item_list:
+            yield badge_id, item
+
+
 @viewer.route('/')
 def home():
-    names = set()
-    for item_list in grants.itervalues():
-        for item in item_list:
-            names.add(item['person_name'])
+    names = set(i['person_name'] for b, i in all_badges())
     return flask.render_template('home.html', names=sorted(names))
+
+
+@viewer.route('/<person_name>')
+def person(person_name):
+    granted = [badges[b]['_template'].render(**i)
+               for b, i in all_badges()
+               if i['person_name'] == person_name]
+    if not granted:
+        flask.abort(404)
+    return flask.render_template('person.html', **{
+        'person_name': person_name,
+        'badges': granted,
+    })
